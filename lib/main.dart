eb63c9bb3e55f2f4412622d637de19ed665bacc8
Chandini Mahalakshmi <104729699+ychandiniml@users.noreply.github.com>
2024-03-31 21:25:51 +0530

    added navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/pages/bottomnav.dart';
 import 'package:fooddeliveryapp/pages/home.dart';
 
 void main() {
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: BottomNav(),
     );
   }
 }
